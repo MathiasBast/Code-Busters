8816aea0b13dda913fe6fbcefedf2190b70b8870
Ellora Virtue <ellora.virtue@hotmail.com>
2020-04-10 13:58:08 +1200

    Add tiny bit of styling

--- client/components/Fail.jsx
+++ client/components/Fail.jsx
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom'
 
 const Fail = () => {
   return (
-<div align="center" className="fail container">
+    <div align="center" className="fail container">
       <h1>Too bad you failed your mission with the ghost!</h1>
-      <p><Link to='/game/find'>Click here to try again</Link></p>
+      <p>Click <Link to='/game/find' id='yellow'>here</Link> to try again</p>
       <p>Good Luck!</p>
       <img src="https://media3.picsearch.com/is?n5HVAqxhGXFWrJ_iOGy2Vl7PKFiGt3a2jl22m0DOnpc&height=341" alt="scare1"/>
       <img src="https://media1.picsearch.com/is?lujw47Zq_GZQhda8J9lpCbg9MRgKKehHBFJgforYv_E&height=227" alt="baby crying"/>
@@ -14,15 +14,5 @@ const Fail = () => {
     </div>
   )
 }
-
-
-
-
-
-
-
-
-
-
 
 export default Fail
--- client/components/Success.jsx
+++ client/components/Success.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
 const Success = () => {
-  return(
+  return (
     <div align="center" className="success container">
       <h1>Well Done</h1>
       <p>You have successfully helped the ghost</p>
       <p>It has now found its true calling!</p>
-        <img src="https://i.kinja-img.com/gawker-media/image/upload/c_scale,f_auto,fl_progressive,q_80,w_1600/blg6zu7taguihvt84brn.jpg" width="700" height="323" alt="ghost whopper"/>
+      <img src="https://i.kinja-img.com/gawker-media/image/upload/c_scale,f_auto,fl_progressive,q_80,w_1600/blg6zu7taguihvt84brn.jpg" width="700" height="323" alt="ghost whopper"/>
     </div>
   )
 }
