20144bdd943c221213d1675c52f02009a9c8e9d4
Ellora Virtue <ellora.virtue@hotmail.com>
2020-06-08 11:20:33 +1200

    Change into a class-based component so state can be used

--- client/components/Hints.jsx
+++ client/components/Hints.jsx
@@ -1,19 +1,25 @@
-
 import React from 'react'
 import { Link, Route } from 'react-router-dom'
 
 import ShowHint from './ShowHint'
 
-const Hints = props => {
-  // '/#/game/:page'
-  // '/#/success'
-  const {name} = props.match.params
-  return (
+class Hints extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      showHint: false
+    }
+  }
+
+  render () {
+    const { name } = this.props.match.params
+    return (
     <>
       <Link to={`/game/${name}/hint`}>Need a hint?</Link>
       <Route path='/game/:page/hint' component={ShowHint} />
     </>
-  )
+    )
+  }
 }
 
 export default Hints
