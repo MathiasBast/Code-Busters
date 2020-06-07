802e7b2bf18c35fd995b8cb4a1b1ec690e0047a5
Ellora Virtue <ellora.virtue@hotmail.com>
2020-06-08 11:27:00 +1200

    Add ability to click 'need a hint' text again to make hint disappear

--- client/components/Hints.jsx
+++ client/components/Hints.jsx
@@ -11,11 +11,27 @@ class Hints extends React.Component {
     }
   }
 
+  handleClick = () => {
+    if (this.state.showHint === false) {
+      this.setState({
+        showHint: true
+      })
+    } else {
+      this.setState({
+        showHint: false
+      })
+    }
+  }
+
   render () {
     const { name } = this.props.match.params
+    const { showHint } = this.state
     return (
     <>
-      <Link to={`/game/${name}/hint`}>Need a hint?</Link>
+      {showHint
+        ? <Link onClick={this.handleClick} to={`/game/${name}`}>Need a hint?</Link>
+        : <Link onClick={this.handleClick} to={`/game/${name}/hint`}>Need a hint?</Link>
+      }
       <Route path='/game/:page/hint' component={ShowHint} />
     </>
     )
